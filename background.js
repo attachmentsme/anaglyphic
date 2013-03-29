e32ecc3043c2bfdb413d1494929772885cf53329
Dan Reed <dan@danreed.net>
2013-03-29 16:55:09 -0700

    anaglyphic is working for text now

--- background.js
+++ background.js
@@ -3,13 +3,12 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 function anaglyphPageOnTab(tab) {
-  console.log('3d-izing the page')
-  chrome.tabs.executeScript(null, 
-  {
-    code:"document.body.appendChild(document.createElement('script')).src='" + chrome.extension.getURL("javascript/anaglyphic.js") +"';"
-  }, function () {});
-  chrome.tabs.executeScript(null, 
-  {
-    code:"document.body.appendChild(document.createElement('script')).src='" + chrome.extension.getURL("javascript/third_party/jquery.min.js") +"';"
-  });
+  // var link = document.createElement('link');
+  // link.type = 'text/css';
+  // link.rel = 'stylesheet';
+  // link.href = chrome.extension.getURL("stylesheets/anaglyphic.css");
+  
+  chrome.tabs.executeScript(null, {code:"document.body.appendChild(document.createElement('script')).src='" + chrome.extension.getURL("javascript/anaglyphic.js") +"';"});
+  chrome.tabs.executeScript(null, {code:"document.body.appendChild(document.createElement('script')).src='" + chrome.extension.getURL("javascript/third_party/jquery.min.js") +"';"});
+  //chrome.tabs.executeScript(null, {code:"document.body.appendChild("+ link + ")';"}, function() {});
 }
--- javascript/anaglyphic.js
+++ javascript/anaglyphic.js
@@ -6,15 +6,46 @@
   }
 
   this.anaglyphPage = function() {
+    var maxDomDepth = _this.findDomDepth();
+    $('body *').each(function(element) {
+      var itemDomDepth = $(this).parents().length + 1;
+      _this.applyAnaglyphClassToElement($(this), _this.normalizeAnaglyphLevel(itemDomDepth, maxDomDepth));
+    });
+    _this.applyStyling();
+  }
+
+  this.findDomDepth = function() {
     var treeDepth = 0;
     $('body *').each(function(element) {
-      //console.log($(this))
-      console.log($(this).parents().length)
       if($(this).parents().length > treeDepth) {
-        treeDepth = $(this).parents().length;
+        treeDepth = $(this).parents().length + 1;
       }
     });
-    console.log(treeDepth)
+    return treeDepth;
+  }
+
+  this.applyAnaglyphClassToElement = function(element, level) {
+    element.addClass('anaglyphic_' + level);
+  }
+
+  this.normalizeAnaglyphLevel = function(itemDomDepth, maxDomDepth) {
+    normalizedLevel = Math.floor(((32.0 / maxDomDepth) * itemDomDepth) - 16.0);
+    if (normalizedLevel > 16) normalizedLevel = 16;
+    if (normalizedLevel < -16) normalizedLevel = -16;
+    return normalizedLevel;
+  }
+
+  this.applyStyling = function() {
+    $( "[class^=anaglyphic]" ).each(function() {
+      var classNames = $(this).attr("class");
+      $(this).css( "color", "rgba(0,255,255,0.5)" );
+      $(this).css( "text-shadow", "rgba(255,0,0,0.5) " + classNames.split("_")[1] + "px 0px 0px");
+    });
+    $( "img[class^=anaglyphic]" ).each(function() {
+      $(this).css("opacity", "0.5")
+      $(this).wrap('<div class="tintBlue"></div>');  
+      $(this).clone().insertAfter(this).wrap('<div class="tintRed"></div>');
+    });
   }
 
   this.init();
